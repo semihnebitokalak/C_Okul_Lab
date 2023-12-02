2db568aed946d6761284b92d6ca9b99883e3d729
GitHub Kullanıcı Adınız <GitHub E-posta Adresiniz>
2023-12-02 15:08:49 +0300

    Burada iki sayıyı topladık

---
+++ 2_İkisayinintop.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+int a,b,toplam;
+
+printf("Lütfen a değerini giriniz.");
+scanf("%d",&a);
+printf("Lütfen b değerini giriniz.");
+scanf("%d",&b);
+
+toplam = a + b ;
+
+printf("Toplam değeriniz = %d\n",toplam);
+
+    return 0;
+}
