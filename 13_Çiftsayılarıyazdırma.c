8b48ef3bf39e679f5662ab3d78299e8939152098
Semih Nebi Tokalak <112804347+semihnebitokalak@users.noreply.github.com>
2023-12-06 16:06:31 +0300

    İki değer arasındaki çift sayıları ekrana yazdırdık

---
+++ 13_Çiftsayılarıyazdırma.c
@@ -0,0 +1,17 @@
+// 1000 ile 2000 arasındaki çift saayıları yazdırma.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int i;
+
+for ( i = 1000; i < 2000; i = i+2 )
+{
+    printf("%d\n",i);
+}
+
+
+    return 0;
+}
